938b5f8903040bc7369982359abb9de2d5354aa9
Arush Sharma <arush3339@gmail.com>
2023-12-14 12:06:56 +0530

    Complete signup page

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -2,34 +2,48 @@
 
 import React from "react";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import Signup from "./Signup";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+import { Subscript } from "lucide-react";
+
+const poppins = Poppins({ weight: ["400", "500"], subsets: ["latin"] });
 
 function Login() {
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <form className="flex shadow-[10px_10px_100px_rgba(49,96,162,0.8)] gap-6 text-white flex-col items-center justify-center w-[350px] md:w-[500px] h-[500px] rounded-md bg-[#001C43]">
-        <Image src="/logo.png" width="160" height="50" alt="logo" />
+    <div className="relative h-screen w-full flex items-center justify-center">
+      <div
+        className={cn(
+          poppins.className,
+          "cursor-pointer hover:scale-105 duration-500 absolute top-6 left-6 font-bold text-4xl text-purple-500 drop-shadow-lg shadow-purple-900"
+        )}
+      >
+        Makemates
+      </div>
+
+      <form className="flex shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] bg-white/40 gap-6 flex-col items-center justify-center px-8 py-6 rounded-sm ">
+        <div className="text-2xl font-light ">Join us now</div>
         <div className="flex flex-col gap-1 w-[300px] md:w-[350px]">
-          <label htmlFor="email" className="ml-2">
+          <label htmlFor="email" className="ml-2 text-sm">
             Email Id :
           </label>
           <input
             type="text"
             name="email"
-            className="w-full rounded-2xl px-3   py-2 bg-[#DDECFF] text-black outline-none focus-within:ring-2"
+            className="w-full rounded-lg placeholder:text-black/70 px-3 py-2 ring-1 bg-transparent text-black focus-within:ring-2"
             placeholder="Enter your email address"
           />
         </div>
         <div className="flex flex-col gap-1 w-[300px] md:w-[350px]">
-          <label htmlFor="email" className="ml-2">
+          <label htmlFor="email" className="ml-2 text-sm">
             Password :
           </label>
           <input
             type="password"
             name="password"
-            className="w-full rounded-2xl px-2 py-2 bg-[#DDECFF] text-black outline-none focus-within:ring-2"
+            className="w-full rounded-lg placeholder:text-black/70 px-2 py-2  ring-1 bg-transparent text-black focus-within:ring-2"
             placeholder="Enter your password"
           />
         </div>
@@ -37,7 +51,7 @@ function Login() {
         <motion.button
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          className="rounded-lg bg-[#A5D7E8] text-blue-950 px-12 py-1 text-md font-bold"
+          className="rounded-lg bg-purple-100 ring-1 hover:ring-6 ring-purple-200 text-black px-12 py-1 text-md font-bold"
         >
           Login
         </motion.button>
@@ -51,11 +65,16 @@ function Login() {
         </motion.button>
         <p className="font-light text-sm">
           {`Don't`} have any account?{" "}
-          <Link href="" className="text-blue-300 font-medium">
+          <Link
+            href=""
+            onClick={() => document.getElementById("signupModal").showModal()}
+            className="text-blue-800 font-medium"
+          >
             Create here
           </Link>
         </p>
       </form>
+      <Signup />
     </div>
   );
 }
